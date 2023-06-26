3a9caa9bf877ddbdc736a6697e907afd1448ee3e
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-06-26 20:41:10 +0100

    Object Casting

---
+++ training/Casting.java
@@ -0,0 +1,14 @@
+public class Casting {
+    
+    public static void casting() {
+
+        final float goldNum = 1.7f;
+        float value1 = 18.14f;
+        int result;
+
+        result = (int) (value1 / goldNum);
+
+        System.out.println("\nResult is " + result);
+
+    }
+}
