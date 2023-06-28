ab73dd90259f24fee987eb6fab7ffd8b30d80cd0
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-06-28 03:30:17 +0100

    Maven projets

---
+++ HelloWord/src/com/openclassrooms/HelloWord.java
@@ -0,0 +1,10 @@
+package com.openclassrooms;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("J'utilise Eclipse !");
+	}
+
+}
